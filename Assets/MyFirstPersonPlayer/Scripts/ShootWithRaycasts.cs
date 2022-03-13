7960b019505431ed77e5a329d57b46808a33565c
Jaden Pleasants <jadenpleasants@fastmail.com>
2022-03-13 16:19:03 -0500

    schuut

---
+++ Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycasts.cs
@@ -0,0 +1,20 @@
+/*
+ * Jaden Pleasants
+ * Assignment 5B
+ * Shoots stuff with raycasts
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycasts : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
