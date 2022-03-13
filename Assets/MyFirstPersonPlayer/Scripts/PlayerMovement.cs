8f282d1d5cbb7bd5be5b99e96dedad99da0f710d
Jaden Pleasants <jadenpleasants@fastmail.com>
2022-03-13 15:39:45 -0500

    Made gravity work

--- Assets/MyFirstPersonPlayer/Scripts/PlayerMovement.cs
+++ Assets/MyFirstPersonPlayer/Scripts/PlayerMovement.cs
@@ -15,6 +15,11 @@ public class PlayerMovement : MonoBehaviour {
     public float gravity = -9.81f;
     public float gravityMultiplier = 2f;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
     private void Awake() {
         gravity *= gravityMultiplier;
         controller = GetComponent<CharacterController>();
@@ -27,6 +32,11 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
